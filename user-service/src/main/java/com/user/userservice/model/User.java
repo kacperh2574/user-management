5cf0575fc5d788c5df509e3962d7e6ebb2865290
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-03-21 19:26:29 +0100

    Create User model

---
+++ user-service/src/main/java/com/user/userservice/model/User.java
@@ -0,0 +1,33 @@
+package com.user.userservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull
+    private String address;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotNull
+    private LocalDate dateOfRegistration;
+}
