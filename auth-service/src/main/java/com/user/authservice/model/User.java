c3a83a0eb267a4b28bdb4d90fc254616f7810ac8
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-03-27 17:26:10 +0100

    Add User model and example data

---
+++ auth-service/src/main/java/com/user/authservice/model/User.java
@@ -0,0 +1,30 @@
+package com.user.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
