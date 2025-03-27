214b4b88c3156cc90a0908514a1f607347da3595
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-03-27 20:36:31 +0100

    Add User model and login DTOs

--- auth-service/src/main/java/com/user/authservice/model/User.java
+++ auth-service/src/main/java/com/user/authservice/model/User.java
@@ -1,15 +1,13 @@
 package com.user.authservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.UUID;
 
 @Getter
 @Setter
+@Builder(toBuilder = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
