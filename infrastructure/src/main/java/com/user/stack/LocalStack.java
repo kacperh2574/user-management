09317d497884dc454de943f7642bfa4cd44f3249
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-04-02 12:29:09 +0200

    Add VPC

--- infrastructure/src/main/java/com/user/stack/LocalStack.java
+++ infrastructure/src/main/java/com/user/stack/LocalStack.java
@@ -4,13 +4,18 @@ import software.amazon.awscdk.*;
 import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
 import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.rds.*;
 import software.amazon.awscdk.services.route53.CfnHealthCheck;
 
 public class LocalStack extends Stack {
 
+    private final Vpc vpc;
+
     public LocalStack(final App scope, final String id, final StackProps props) {
         super(scope, id, props);
+
+        this.vpc = createVpc();
 
         DatabaseInstance userServiceDb = createDatabase("UserServiceDB", "user-service-db");
         DatabaseInstance authServiceDb = createDatabase("AuthServiceDB", "auth-service-db");
@@ -19,12 +24,20 @@ public class LocalStack extends Stack {
         CfnHealthCheck authDbHealthCheck = createDbHealthCheck(authServiceDb, "AuthServiceDBHealthCheck");
     }
 
+    private Vpc createVpc() {
+        return Vpc.Builder.create(this, "UserManagementVPC")
+                .vpcName("UserManagementVPC")
+                .maxAzs(2)
+                .build();
+    }
+
     private DatabaseInstance createDatabase(final String id, final String dbName) {
         return DatabaseInstance.Builder.create(this, id)
                 .engine(DatabaseInstanceEngine.postgres(
                         PostgresInstanceEngineProps.builder()
                                 .version(PostgresEngineVersion.VER_17_2)
                                 .build()))
+                .vpc(vpc)
                 .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
                 .allocatedStorage(20)
                 .credentials(Credentials.fromGeneratedSecret("admin_user"))
