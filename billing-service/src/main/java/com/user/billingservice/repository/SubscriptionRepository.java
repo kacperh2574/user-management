ca6dabb2c9d6ce760dd166d1fba09db331cdf60e
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-04-13 13:13:59 +0200

    Add subscription repo and service

---
+++ billing-service/src/main/java/com/user/billingservice/repository/SubscriptionRepository.java
@@ -0,0 +1,11 @@
+package com.user.billingservice.repository;
+
+import com.user.billingservice.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, UUID> {
+    Optional<Subscription> findByUserId(UUID userId);
+}
