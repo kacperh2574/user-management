925c62eea67873c908182480924e32669312dc82
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-04-13 20:46:32 +0200

    Adjust GRPC server to create subscriptions

--- billing-service/src/main/java/com/user/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/user/billingservice/grpc/BillingGrpcService.java
@@ -3,25 +3,46 @@ package com.user.billingservice.grpc;
 import billing.BillingRequest;
 import billing.BillingResponse;
 import billing.BillingServiceGrpc.BillingServiceImplBase;
+import com.user.billingservice.dto.SubscriptionRequestDTO;
+import com.user.billingservice.dto.SubscriptionResponseDTO;
+import com.user.billingservice.model.PlanType;
+import com.user.billingservice.service.SubscriptionService;
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.UUID;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
 
-    @Override
-    public void createBillingAccount(BillingRequest billingRequest, StreamObserver<BillingResponse> responseObserver) {
+    private final SubscriptionService subscriptionService;
 
-        log.info("createBillingAccount requested received {}", billingRequest.toString());
+    public BillingGrpcService(SubscriptionService subscriptionService) {
+        this.subscriptionService = subscriptionService;
+    }
+
+    @Override
+    public void createSubscription(BillingRequest request, StreamObserver<BillingResponse> responseObserver) {
+        log.info("Received createSubscription request: {}", request);
+
+        UUID userId = UUID.fromString(request.getUserId());
+
+        SubscriptionRequestDTO subscriptionRequest = SubscriptionRequestDTO.builder()
+                .planType(PlanType.valueOf(request.getPlanType()))
+                .build();
+
+        SubscriptionResponseDTO subscription = subscriptionService.createSubscription(userId, subscriptionRequest);
 
         BillingResponse response = BillingResponse.newBuilder()
-                .setAccountId("12345")
-                .setStatus("Active")
+                .setSubscriptionId(subscription.getId())
+                .setStatus(subscription.getStatus().toString())
                 .build();
+
+        log.info("Subscription created: {}", response);
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
