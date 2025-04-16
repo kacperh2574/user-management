949f6de35c5d1af26cbbef548c06d3c05b4e1ff8
kacperh2574 <hanuszewicz.kacper@gmail.com>
2025-04-16 22:45:34 +0200

    Set test profile for Stripe controller tests

--- billing-service/src/test/java/com/user/billingservice/controller/integration/StripeControllerTest.java
+++ billing-service/src/test/java/com/user/billingservice/controller/integration/StripeControllerTest.java
@@ -9,11 +9,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+@ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StripeControllerTest {
 
